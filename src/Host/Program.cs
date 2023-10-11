07ef6b97e13c7ff6b59259967b023c5c5f1c22a6
urmatgit <urmat2g@gmail.com>
2023-10-11 15:29:48 +0300

    add port

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -6,27 +6,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 var allowOrigins = builder.Configuration.GetValue<string>("AllowOrigins");
-builder.Services.AddCors(options =>
-{
-options.AddPolicy("CorsPolicy", builder =>
-{
-    builder.WithOrigins(allowOrigins)
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowAnyOrigin()
-      .AllowCredentials();
+builder.Services.AddCors();
+//    options =>
+//{
+//options.AddPolicy("CorsPolicy", builder =>
+//{
+//    builder.WithOrigins(allowOrigins)
+//        .AllowAnyHeader()
+//        .AllowAnyMethod()
+//        .AllowAnyOrigin()
+//      .AllowCredentials();
 
-});
+//});
     
     
-    options.AddPolicy("AllowHeaders", builder =>
-    {
-        builder.WithOrigins(allowOrigins)
-                .WithHeaders(HeaderNames.ContentType, HeaderNames.Server, HeaderNames.AccessControlAllowHeaders, HeaderNames.AccessControlExposeHeaders, "x-custom-header", "x-path", "x-record-in-use", HeaderNames.ContentDisposition);
-    });
-});
+//    options.AddPolicy("AllowHeaders", builder =>
+//    {
+//        builder.WithOrigins(allowOrigins)
+//                .WithHeaders(HeaderNames.ContentType, HeaderNames.Server, HeaderNames.AccessControlAllowHeaders, HeaderNames.AccessControlExposeHeaders, "x-custom-header", "x-path", "x-record-in-use", HeaderNames.ContentDisposition);
+//    });
+//});
 var app = builder.Build();
-
+app.UseCors(options =>
+{
+    options.WithOrigins("http://http://194.190.152.99:80");
+});
+//app.UseCors("CorsPolicy");
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -49,8 +54,9 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapRazorPages();
+
 app.MapControllers();
-app.UseCors("CorsPolicy");
+
 app.MapFallbackToFile("index.html");
 
 app.Run();
