261879c04895e923c7a27327bd59e4bcbcd5e660
urmatgit <urmat2g@gmail.com>
2023-10-11 09:47:22 +0300

    add cors

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -1,9 +1,26 @@
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
-
+var allowOrigins = builder.Configuration.GetValue<string>("AllowOrigins");
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins(allowOrigins)
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+          .AllowCredentials();
+    });
+    options.AddPolicy("AllowHeaders", builder =>
+    {
+        builder.WithOrigins(allowOrigins)
+                .WithHeaders(HeaderNames.ContentType, HeaderNames.Server, HeaderNames.AccessControlAllowHeaders, HeaderNames.AccessControlExposeHeaders, "x-custom-header", "x-path", "x-record-in-use", HeaderNames.ContentDisposition);
+    });
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,13 +33,15 @@ else
     app.UseHsts();
     app.UseForwardedHeaders(new ForwardedHeadersOptions
     {
-        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto 
     });
+    
 }
 
 app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
+app.UseCors("CorsPolicy");
 app.UseRouting();
 
 app.MapRazorPages();
