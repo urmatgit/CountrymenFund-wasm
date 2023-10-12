cdd77b9b7fce187fc37a14ee5de54198f36dea5f
urmatgit <urmat2g@gmail.com>
2023-10-12 09:35:32 +0300

    new setting

--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -6,32 +6,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 var allowOrigins = builder.Configuration.GetValue<string>("AllowOrigins");
-builder.Services.AddCors();
-//    options =>
-//{
-//options.AddPolicy("CorsPolicy", builder =>
-//{
-//    builder.WithOrigins(allowOrigins)
-//        .AllowAnyHeader()
-//        .AllowAnyMethod()
-//        .AllowAnyOrigin()
-//      .AllowCredentials();
-
-//});
-    
-    
-//    options.AddPolicy("AllowHeaders", builder =>
-//    {
-//        builder.WithOrigins(allowOrigins)
-//                .WithHeaders(HeaderNames.ContentType, HeaderNames.Server, HeaderNames.AccessControlAllowHeaders, HeaderNames.AccessControlExposeHeaders, "x-custom-header", "x-path", "x-record-in-use", HeaderNames.ContentDisposition);
-//    });
-//});
-var app = builder.Build();
-app.UseCors(options =>
+builder.Services.AddCors(options =>
 {
-    options.WithOrigins("http://http://194.190.152.99:80");
+    
+    options.AddPolicy("CorsPolicy", builder =>
+    
+        builder.WithOrigins(allowOrigins)
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        //.AllowAnyOrigin()
+      .AllowCredentials()
+
+    );
+
+
+    options.AddPolicy("AllowHeaders", builder =>
+    {
+        builder.WithOrigins(allowOrigins)
+                .WithHeaders(HeaderNames.ContentType, HeaderNames.Server, HeaderNames.AccessControlAllowHeaders, HeaderNames.AccessControlExposeHeaders, "x-custom-header", "x-path", "x-record-in-use", HeaderNames.ContentDisposition);
+    });
 });
-//app.UseCors("CorsPolicy");
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -39,12 +36,12 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-  //  app.UseHsts();
+    //  app.UseHsts();
     //app.UseForwardedHeaders(new ForwardedHeadersOptions
     //{
     //    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto 
     //});
-    
+
 }
 
 //app.UseHttpsRedirection();
